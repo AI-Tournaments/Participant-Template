e8598a62617e28b210b8c38bd27e88d75ea99b6c
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-12-27 23:24:02 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,3 +1,9 @@
-onmessage = messageEvent => {
-	postMessage('Response');
+onmessage = messageEvent => { /** Arena init. */
+	console.log(messageEvent.data.settings);
+	console.log(messageEvent.data.opponents);
+	onmessage = tock;
+	postMessage(null);
+}
+function tock(messageEvent){ /** Round response. */
+	postMessage('response');
 }
