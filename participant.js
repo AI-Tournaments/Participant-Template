684a3ae0c09b4ccf2f8e214705490d8da19e010c
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-01-04 14:16:41 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,4 +1,6 @@
+let team;
 onmessage = messageEvent => { /** Arena init. */
+	team = messageEvent.data.opponents.findIndex(opponent=>opponent===null);
 	console.log(messageEvent.data.settings);
 	console.log(messageEvent.data.opponents);
 	onmessage = tock;
