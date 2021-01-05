4a4770af5b938cfa12b3804fdc61d986b9dd9af2
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-01-05 11:36:53 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,10 +1,19 @@
 let team;
-onmessage = messageEvent => { /** Arena init. */
+onmessage = ()=>{
+	onmessage = loadArenaData;
+	postMessage(null); /** Dependencies load completed, http requests can be disabled after this point. */
+}
+function loadArenaData(messageEvent){ /** Initiated participant. */
 	team = messageEvent.data.opponents.findIndex(opponent=>opponent===null);
 	console.log(messageEvent.data.settings);
 	console.log(messageEvent.data.opponents);
 	onmessage = tock;
-	postMessage(null);
+	if(messageEvent.data === ''){
+		messageEvent.data.secret = null;
+		postMessage(messageEvent.data); /** Data to participant.js siblings, if used by the arena. */
+	}else{
+		postMessage(null);
+	}
 }
 function tock(messageEvent){ /** Round response. */
 	postMessage('response');
