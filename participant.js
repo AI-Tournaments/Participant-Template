8cff3f9f84645eb64b6b735d6a2c13c047d31f6d
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-01-13 17:07:42 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,9 +1,5 @@
 let team;
 onmessage = ()=>{
-	onmessage = loadArenaData;
-	postMessage(null); /** Dependencies load completed, http requests can be disabled after this point. */
-}
-function loadArenaData(messageEvent){ /** Initiated participant. */
 	team = messageEvent.data.opponents.findIndex(opponent=>opponent===null);
 	console.log(messageEvent.data.settings);
 	console.log(messageEvent.data.opponents);
