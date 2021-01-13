1040ef0364ca3f0807fc19277c52106be0a2770c
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-01-13 17:21:42 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,15 +1,9 @@
 let team;
-onmessage = ()=>{
+onmessage = ()=>{ /** Initiate participant. */
+	onmessage = tock;
 	team = messageEvent.data.opponents.findIndex(opponent=>opponent===null);
 	console.log(messageEvent.data.settings);
 	console.log(messageEvent.data.opponents);
-	onmessage = tock;
-	if(messageEvent.data === ''){
-		messageEvent.data.secret = null;
-		postMessage(messageEvent.data); /** Data to participant.js siblings, if used by the arena. */
-	}else{
-		postMessage(null);
-	}
 }
 function tock(messageEvent){ /** Round response. */
 	postMessage('response');
