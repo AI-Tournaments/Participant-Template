89357233dfb407da671a496860e3c7e5a8428dda
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-06-22 08:30:40 +0200

    Update participant.js
    Signed-off-by: Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>

--- participant.js
+++ participant.js
@@ -1,15 +1,13 @@
 let _team;
 let _settings;
 let _opponents;
-ParticipantHelper.init = (settings, opponents) => {
-	_team = opponents.findIndex(opponent=>opponent===null);
-	_settings = settings;
-	_opponents = opponents;
-	console.log(_settings);
-	console.log(_opponents);
-}
-ParticipantHelper.onmessage = (data, type) => {
-	if(type === 'post'){
-		ParticipantHelper.respond('Round response');
+onmessage = init => {
+	_settings = init.settings;
+	_opponents = init.opponents;
+	_team = _opponents.findIndex(opponent=>opponent===null);
+	onmessage = message => {
+		if(type === 'post'){
+			postMessage('Post response');
+		}
 	}
 }
