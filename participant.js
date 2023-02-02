5c11b6926441887bfaba20524c5793f2357a1b24
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2023-02-02 22:10:15 +0100

    Update participant.js
    Signed-off-by: Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>

--- participant.js
+++ participant.js
@@ -1,10 +1,7 @@
-let _team;
-let _settings;
-let _opponents;
 onmessage = init => {
-	_settings = init.settings;
-	_opponents = init.opponents;
-	_team = _opponents.findIndex(opponent=>opponent===null);
+	const _settings = init.settings;
+	const _opponents = init.opponents;
+	const _team = _opponents.findIndex(opponent=>opponent===null);
 	onmessage = message => {
 		if(message.type === 'Post'){
 			postMessage('YOUR_RESPONSE');
